c9100873d49f4a4e2c4a6425c170222868602860
112761 <112761@koreainvestment.com>
2024-10-14 18:24:36 +0900

    이벤트 상세페이지 이미지 세팅 및 초기 개발

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import {React} from 'react';
+import { useParams, Link } from 'react-router-dom';
+import '../styles/EventDetail.css';
 
 const eventDetails = {
-    1: { title: "$30 신청 혜택", description: "신청하면 즉시 $30 지급되는 이벤트입니다.", dateRange: "2024.09.27 ~ 2024.12.31" },
-    2: { title: "해외주식 거래", description: "매주 해외주식 거래를 통해 선물을 받을 수 있습니다.", dateRange: "2024.10.07 ~ 2024.11.15" },
-    3: { title: "공모주 골든위크", description: "10월 골든위크 기간 중 공모주 청약을 확인하세요.", dateRange: "2024.10.01 ~ 2024.10.31" }
+    1: { title: "$30 신청 혜택", dateRange: "2024.09.27 ~ 2024.12.31", imgName : "img_bankis_ff_30dollar_2410" },
+    2: { title: "해외주식 거래", dateRange: "2024.10.07 ~ 2024.11.15", imgName : "23161753img_fs_exchange_plus_event_2408" },
+    3: { title: "BanKIS 계좌개설 이벤트", dateRange: "2024.10.01 ~ 2024.12.31", imgName : "img_bankis_direct_event_2212" }
 };
 
 function EventDetail() {
     const { id } = useParams();
     const event = eventDetails[id];
+    const imageUrl = "https://file.truefriend.com/updata/namo/"+eventDetails[id].imgName+".png"
 
     if (!event) {
         return <p>해당 이벤트를 찾을 수 없습니다.</p>;
@@ -18,8 +20,10 @@ function EventDetail() {
     return (
         <div className="event-detail">
             <h2>{event.title}</h2>
-            <p>{event.description}</p>
             <p>{event.dateRange}</p>
+            <div>
+                <img id = "detailImage" src={imageUrl}/>
+            </div>
         </div>
     );
 }
