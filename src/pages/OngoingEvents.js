3a25b0ac2a8db6baf9cf6c6e24f18d7606b6a295
112761 <112761@koreainvestment.com>
2024-10-15 07:58:00 +0900

    혜택탭에서의 신규 계좌개설 이벤트 랜딩을 위해 임의 수정

--- src/pages/OngoingEvents.js
+++ src/pages/OngoingEvents.js
@@ -4,7 +4,7 @@ import EventCard from '../components/EventCard';
 const ongoingEventList = [
     { id: 1, title: "$30 신청 혜택", description: "신청하면 즉시 $30 지급", dateRange: "2024.09.27 ~ 2024.12.31" },
     { id: 2, title: "해외주식 거래", description: "매주 선물 받기!", dateRange: "2024.10.07 ~ 2024.11.15" },
-    { id: 3, title: "공모주 골든위크", description: "10월 캘린더 확인하기", dateRange: "2024.10.01 ~ 2024.10.31" }
+    { id: 3, title: "BanKIS 계좌개설 이벤트", description: "모두 다! 드려요!", dateRange: "2024.10.01 ~ 2024.12.31" }
 ];
 
 function OngoingEvents() {
