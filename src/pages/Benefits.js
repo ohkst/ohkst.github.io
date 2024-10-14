0d68638dc1259942258f632578d7461443aba4ca
112761 <112761@koreainvestment.com>
2024-10-14 18:21:55 +0900

    혜택탭 페이지 및 버튼 랜딩 개발

--- src/pages/Benefits.js
+++ src/pages/Benefits.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useNavigate, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '../styles/Benefit.css';
 
 function Benefits() {
+    const navigate = useNavigate();
+    const handleClick = id =>{
+        navigate('/event/'+id);
+    };
+
     return (
         <div className="benefits">
-            <h3>혜택 탭</h3>
-            <p>이 탭에서는 다양한 혜택을 확인할 수 있습니다.</p>
-            {/* 혜택 관련 콘텐츠 추가 */}
+            <div id = "benefitBg">
+                <div id = "btnGroup">
+                    <button className="btn1" type="button" onClick={() => handleClick("2")}></button>
+                    <button className="btn2" type="button" onClick={() => window.open("https://m.koreainvestment.com/app/mtsrenewal.jsp?type=06&SSO_SCREENNO=3911")}></button>
+                    <button className="btn3" type="button" onClick={() => handleClick("3")}></button>
+                    <button className="btn4" type="button" onClick={() => handleClick("3")}></button>
+                    <button className="btn5" type="button">미참가중</button>
+                    {/* TODO api로 참가여부 받아와서 변경해줘야 함 */}
+                </div>
+                {/* 혜택 관련 콘텐츠 추가 */}
+            </div>
         </div>
     );
 }
