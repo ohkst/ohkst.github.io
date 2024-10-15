6e2c6ecb22caeab529bbaaf231267b7cac947fb4
112761 <112761@koreainvestment.com>
2024-10-15 16:52:51 +0900

    이벤트 상세페이지 - 내용보기,참여하기 페이지 개발

---
+++ src/pages/EventContent.js
@@ -0,0 +1,23 @@
+import {React} from 'react';
+import { useParams } from 'react-router-dom';
+import '../styles/EventContent.css';
+
+const eventDetails = {
+    1: { title: "Bankis 해외주식 신규 $30 이벤트",imgName : "img_bankis_ff_30dollar_2410" },
+    2: { title: "해외주식 거래", imgName : "23161753img_fs_exchange_plus_event_2408" },
+    3: { title: "BanKIS 계좌개설 이벤트", imgName : "img_bankis_direct_event_2212" }
+};
+
+function EventContent() {
+    const { id } = useParams();
+    const event = eventDetails[id];
+    const imageUrl = "https://file.truefriend.com/updata/namo/"+event.imgName+".png"
+
+    return (
+        <div>
+            <img id = "detailImage" src={imageUrl}/>
+        </div>
+    );
+}
+
+export default EventContent;
