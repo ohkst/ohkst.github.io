a7b0af38445e17edb09ac8d9bede7b4dcaa3ce93
112761 <112761@koreainvestment.com>
2024-10-16 15:34:23 +0900

    혜택탭 css 수정

--- src/pages/Benefits.js
+++ src/pages/Benefits.js
@@ -12,11 +12,11 @@ function Benefits() {
         <div className="benefits">
             <div id = "benefitBg">
                 <div className = "btnGroup">
-                    <button className="btn1" type="button" onClick={() => handleClick("2")}>대상 여부 조회</button>
-                    <button className="btn2" type="button" onClick={() => window.open("https://m.koreainvestment.com/app/mtsrenewal.jsp?type=06&SSO_SCREENNO=3911")}>해외거래서비스 신청하기</button>
-                    <button className="btn3" type="button" onClick={() => handleClick("3")}>이벤트 신청하기</button>
-                    <button className="btn4" type="button" onClick={() => handleClick("3")}>주식 추첨하기</button>
-                    <button className="btn5" type="button">미참가중</button>
+                    <button className="content-btn btn1" type="button" onClick={() => handleClick("2")}>대상 여부 조회</button>
+                    <button className="content-btn btn2" type="button" onClick={() => window.open("https://m.koreainvestment.com/app/mtsrenewal.jsp?type=06&SSO_SCREENNO=3911")}>해외거래서비스 신청하기</button>
+                    <button className="content-btn btn3" type="button" onClick={() => handleClick("3")}>이벤트 신청하기</button>
+                    <button className="content-btn btn4" type="button" onClick={() => handleClick("3")}>주식 추첨하기</button>
+                    <button className="content-btn btn5" type="button">미참가중</button>
                     {/* TODO api로 참가여부 받아와서 변경해줘야 함 */}
                 </div>
                 {/* 혜택 관련 콘텐츠 추가 */}
