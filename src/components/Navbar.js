1b9198fc762aad230c452f41095eaf3c4d9af4e0
112761 <112761@koreainvestment.com>
2024-10-18 10:10:12 +0900

    경로에 따라 네비게이션바 제목 바뀌도록 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import backIcon from '../images/ic_navi_back_24.png'
+import backIcon from '../images/ic_navi_back_24.png';
 
 function Navbar() {
   const navigate = useNavigate();
@@ -10,12 +10,25 @@ function Navbar() {
     navigate(-1);
   };
 
+  const titleMap = {
+    '/': '이벤트',
+    '/event/:id': '이벤트 상세',
+  };
+
+  function getTitleName(path) {
+    if (path.startsWith('/event/')) {
+      return '이벤트 상세';
+    } else {
+      return titleMap[path];
+    }
+  }
+
   return (
     <nav id="naviHeader">
       <span onClick={handleBackButtonClick}>
         <img className="imgNaviBack" src={backIcon} alt="" />
       </span>
-      <li id="brand">이벤트</li>
+      <li id="naviTitle">{getTitleName(window.location.pathname)}</li>
     </nav>
   );
 }
