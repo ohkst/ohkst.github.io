56200baaa72dc824afc4132ef6d1e6ec26cd74d5
김성태 <112251@koreainvestment.com>
2024-10-18 14:30:28 +0900

    API 테스트 요청 추가

--- src/pages/EventContent.js
+++ src/pages/EventContent.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import '../styles/EventContent.css';
 
 const eventDetails = {
+    0: { title: "Bankis 해외주식 신규 $30 이벤트",imgName : "img_bankis_ff_30dollar_2410" },
     1: { title: "Bankis 해외주식 신규 $30 이벤트",imgName : "img_bankis_ff_30dollar_2410" },
     2: { title: "해외주식 거래", imgName : "23161753img_fs_exchange_plus_event_2408" },
     3: { title: "BanKIS 계좌개설 이벤트", imgName : "img_bankis_direct_event_2212" }
