567cad5c50425e8c32531b0e78598a87c9febc06
olaf <olaf@koreainvestment.com>
2024-11-11 13:39:03 +0900

    [김성태] 파일 추가

---
+++ src/tsxtojs/components/Checkbox.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+require("../styles/Checkbox.css");
+function Checkbox({ checked, onChange, iconOn, iconOff, label, labelClass, }) {
+    return (react_1.default.createElement("label", null,
+        react_1.default.createElement("input", { id: "checkbox", type: "checkbox", checked: checked, onChange: onChange }),
+        react_1.default.createElement("img", { id: "checkIcon", src: checked ? iconOn : iconOff, alt: "\uCCB4\uD06C\uBC15\uC2A4" }),
+        react_1.default.createElement("span", { className: labelClass }, label)));
+}
+exports.default = Checkbox;
