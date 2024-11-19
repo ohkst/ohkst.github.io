6f4e232856fb9bf0b684683410e9bd9dcb13f1a0
olaf <olaf@koreainvestment.com>
2024-11-19 13:32:31 +0900

    [김성태] not found 추가

--- src/tsxtojs/App.js
+++ src/tsxtojs/App.js
@@ -8,12 +8,16 @@ const react_router_dom_1 = require("react-router-dom");
 const Navbar_1 = __importDefault(require("./components/Navbar"));
 const Events_1 = __importDefault(require("./pages/Events"));
 const EventDetail_1 = __importDefault(require("./pages/EventDetail"));
+const AwsHealth_1 = __importDefault(require("./pages/AwsHealth"));
+const NotFound_1 = __importDefault(require("./pages/NotFound"));
 function App() {
     return (react_1.default.createElement(react_router_dom_1.BrowserRouter, null,
         react_1.default.createElement("div", { className: "windowFrame" },
             react_1.default.createElement(Navbar_1.default, null),
             react_1.default.createElement(react_router_dom_1.Routes, null,
                 react_1.default.createElement(react_router_dom_1.Route, { path: "/", element: react_1.default.createElement(Events_1.default, null) }),
-                react_1.default.createElement(react_router_dom_1.Route, { path: "/event/:id", element: react_1.default.createElement(EventDetail_1.default, null) })))));
+                react_1.default.createElement(react_router_dom_1.Route, { path: "/event/:id", element: react_1.default.createElement(EventDetail_1.default, null) }),
+                react_1.default.createElement(react_router_dom_1.Route, { path: "/aws/health", element: react_1.default.createElement(AwsHealth_1.default, null) }),
+                react_1.default.createElement(react_router_dom_1.Route, { path: "*", element: react_1.default.createElement(NotFound_1.default, null) })))));
 }
 exports.default = App;
