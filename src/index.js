cd4b3f51796618572679b70679563d2070ef9806
olaf <olaf@koreainvestment.com>
2024-11-21 12:37:05 +0900

    [김성태] iOS safari 환경 설정

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 // styles
 import './index.css';
 import './styles/navigation.css';
