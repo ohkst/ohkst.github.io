915730166b04e69228b91a0dd4c70147a26efed2
olaf <olaf@koreainvestment.com>
2024-11-22 15:57:02 +0900

    [김성태] 펑션 테스트

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,16 +12,18 @@ function Navbar() {
       // Android 네이티브 함수 호출
       console.warn("Android");
       window.Android.showToast("안드로이드 네이티브 호출");
+
     } else if (window.webkit && 
       window.webkit.messageHandlers && 
       window.webkit.messageHandlers.back &&
       window.webkit.messageHandlers.iOS) {
       // iOS 네이티브 함수 호출
       console.warn("iOS");
-      window.webkit.messageHandlers.iOS.postMessage("iOS");
+      // window.webkit.messageHandlers.iOS.postMessage("iOS");
 
-      window.webkit.messageHandlers.back.postMessage("back");
+      // window.webkit.messageHandlers.back.postMessage("back");
       window.webkit.messageHandlers.back.showAlert("t", "m");
+      
     } else {
       console.warn("WebView 환경이 아님");
       navigate(-1);
