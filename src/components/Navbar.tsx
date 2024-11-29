a37b6654d75d6d681e5b6a05bb9e09dd089ec016
김성태 <112251@koreainvestment.com>
2024-11-29 14:54:17 +0900

    tr model

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,37 +32,13 @@ function Navbar() {
       //   window.webkit.messageHandlers.back.postMessage("back");
       // }
 
-      if (window.webkit.messageHandlers.moveEventDetail) {
-        window.webkit.messageHandlers.moveEventDetail.postMessage("");
-      }
+      // if (window.webkit.messageHandlers.moveEventDetail) {
+      //   window.webkit.messageHandlers.moveEventDetail.postMessage("");
+      // }
 
-      if (window.webkit.messageHandlers.needLogin) {
-        window.webkit.messageHandlers.needLogin.postMessage("");
-      }
-
-      if (window.webkit.messageHandlers.getAccountAlias) {
-        window.webkit.messageHandlers.getAccountAlias.postMessage("");
-      }
-
-      if (window.webkit.messageHandlers.getAppNoticeList) {
-        window.webkit.messageHandlers.getAppNoticeList.postMessage("");
-      }
-
-      if (window.webkit.messageHandlers.getMobileNoticePopup) {
-        window.webkit.messageHandlers.getMobileNoticePopup.postMessage("");
-      }
-
-      if (window.webkit.messageHandlers.getAutoTradingCheck) {
-        window.webkit.messageHandlers.getAutoTradingCheck.postMessage("");
-      }
-
-      if (window.webkit.messageHandlers.getBankisStock) {
-        window.webkit.messageHandlers.getBankisStock.postMessage("");
-      }
-
-      if (window.webkit.messageHandlers.getBankisDollar) {
-        window.webkit.messageHandlers.getBankisDollar.postMessage("");
-      }
+      // if (window.webkit.messageHandlers.needLogin) {
+      //   window.webkit.messageHandlers.needLogin.postMessage("");
+      // }
 
       if (window.webkit.messageHandlers.getOverseasStock) {
         window.webkit.messageHandlers.getOverseasStock.postMessage("");
@@ -72,6 +48,78 @@ function Navbar() {
     } else {
       console.warn("Mobile 환경이 아님");
       navigate(-1);
+    }
+  };
+
+  const handleBackButtonClick1 = () => {
+    if (window.webkit && 
+      window.webkit.messageHandlers) {
+      // iOS 네이티브 함수 호출
+      console.warn("iOS");
+
+      if (window.webkit.messageHandlers.getAccountAlias) {
+        window.webkit.messageHandlers.getAccountAlias.postMessage("");
+      }
+    }
+  };
+
+  const handleBackButtonClick2 = () => {
+    if (window.webkit && 
+      window.webkit.messageHandlers) {
+      // iOS 네이티브 함수 호출
+      console.warn("iOS");
+
+      if (window.webkit.messageHandlers.getAppNoticeList) {
+        window.webkit.messageHandlers.getAppNoticeList.postMessage("");
+      }
+    }
+  };
+
+  const handleBackButtonClick3 = () => {
+    if (window.webkit && 
+      window.webkit.messageHandlers) {
+      // iOS 네이티브 함수 호출
+      console.warn("iOS");
+
+      if (window.webkit.messageHandlers.getMobileNoticePopup) {
+        window.webkit.messageHandlers.getMobileNoticePopup.postMessage("");
+      }
+    }
+  };
+
+  const handleBackButtonClick4 = () => {
+    if (window.webkit && 
+      window.webkit.messageHandlers) {
+      // iOS 네이티브 함수 호출
+      console.warn("iOS");
+
+      if (window.webkit.messageHandlers.getAutoTradingCheck) {
+        window.webkit.messageHandlers.getAutoTradingCheck.postMessage("");
+      }
+    }
+  };
+
+  const handleBackButtonClick5 = () => {
+    if (window.webkit && 
+      window.webkit.messageHandlers) {
+      // iOS 네이티브 함수 호출
+      console.warn("iOS");
+
+      if (window.webkit.messageHandlers.getBankisStock) {
+        window.webkit.messageHandlers.getBankisStock.postMessage("");
+      }
+    }
+  };
+
+  const handleBackButtonClick6 = () => {
+    if (window.webkit && 
+      window.webkit.messageHandlers) {
+      // iOS 네이티브 함수 호출
+      console.warn("iOS");
+
+      if (window.webkit.messageHandlers.getBankisDollar) {
+        window.webkit.messageHandlers.getBankisDollar.postMessage("");
+      }
     }
   };
 
@@ -87,6 +135,24 @@ function Navbar() {
   return (
     <nav id="naviHeader">
       <span onClick={handleBackButtonClick}>
+        <img className="imgNaviBack" src={backIcon} alt="" />
+      </span>
+      <span onClick={handleBackButtonClick1}>
+        <img className="imgNaviBack" src={backIcon} alt="" />
+      </span>
+      <span onClick={handleBackButtonClick2}>
+        <img className="imgNaviBack" src={backIcon} alt="" />
+      </span>
+      <span onClick={handleBackButtonClick3}>
+        <img className="imgNaviBack" src={backIcon} alt="" />
+      </span>
+      <span onClick={handleBackButtonClick4}>
+        <img className="imgNaviBack" src={backIcon} alt="" />
+      </span>
+      <span onClick={handleBackButtonClick5}>
+        <img className="imgNaviBack" src={backIcon} alt="" />
+      </span>
+      <span onClick={handleBackButtonClick6}>
         <img className="imgNaviBack" src={backIcon} alt="" />
       </span>
       <li id="naviTitle">{getTitleName()}</li>
