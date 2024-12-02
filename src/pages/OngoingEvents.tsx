ea4ff0c352da8a5a1e72271b9b5d315a01790b1e
김성태 <112251@koreainvestment.com>
2024-12-02 14:25:52 +0900

    이벤트 리스트 TR 연동

--- src/pages/OngoingEvents.tsx
+++ src/pages/OngoingEvents.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 // import { useNavigate } from "react-router-dom";
-import EventCard, { EventCardProps } from "../components/EventCard";
+import EventCard from "../components/EventCard";
 import Banner from "../components/Banner";
 import {
   AccountListModel,
@@ -10,21 +10,16 @@ import {
   BankisStockModel,
   BankisDollarModel,
   OverseasStockModel,
+  EventListItemType
 } from "../API/EventModel";
 
-const APIEndPoint = {
-  GetTest: "posts",
-  PostTest: "posts",
-};
-Object.freeze(APIEndPoint);
-
-interface EventInfo {
-  id: number;
-  imagePath: string;
-  title: string | null;
-  dateRange: string | null;
-  ongoing: string | null;
-}
+// interface EventInfo {
+//   id: number;
+//   imagePath: string;
+//   title: string | null;
+//   dateRange: string | null;
+//   ongoing: string | null;
+// }
 
 interface OngoingEventsProps {
   filterType: string;
@@ -35,22 +30,15 @@ function OngoingEvents({ filterType, filterAvailable }: OngoingEventsProps) {
   // const navigate = useNavigate();
   const [selectedCategory] = useState("all");
 
-  const imageBase = "https://file.truefriend.com/Storage/mobile/event/eventQ";
-  const combinedEvents = Array.from({ length: 20 }, (_, index) => ({
-    id: index + 1,
-    imagePath: `${imageBase}${index + 70}_banner.png`,
-    title: "$30 신청 혜택",
-    dateRange: "2024.09.27 ~ 2024.12.31",
-    ongoing: "진행중",
-  }));
+  const [eventList, setEventList] = useState<EventListItemType[]>([]);
 
   const isBannerVisibleRef = useRef(false);
-  const [events] = useState<EventInfo[]>(combinedEvents);
+  // const [events] = useState<EventInfo[]>(combinedEvents);
 
   const filteredList =
     selectedCategory === "all"
-      ? events
-      : events.filter((events) => events.ongoing === selectedCategory);
+      ? eventList
+      : eventList.filter((event) => event.na_event_type !== selectedCategory);
 
   if (window.Android) {
     // Android 네이티브 함수 호출
@@ -97,6 +85,9 @@ function OngoingEvents({ filterType, filterAvailable }: OngoingEventsProps) {
       try {
         // 간단히 key 값만 파싱
         const { key } = JSON.parse(message);
+        if (key !== "event/mobile_notice_popup") {
+          return;
+        }
         const model = modelMapping[key as ModelKey];
 
         if (!model) {
@@ -105,9 +96,12 @@ function OngoingEvents({ filterType, filterAvailable }: OngoingEventsProps) {
         }
 
         // 필요한 경우 전체 데이터 파싱
-        const parsedData = model.parse(JSON.parse(message));
+        const parsedData = EventListModel.parse(JSON.parse(message));
+        const parsedEventList = parsedData.content
+        setEventList(parsedEventList)
+
         console.log("파싱된 JSON 객체:", parsedData);
-        console.log("content:", parsedData.content);
+        console.log("eventList:", parsedEventList);
         console.log("key:", parsedData.key);
       } catch (error) {
         console.error("JSON 파싱 오류:", error);
@@ -118,7 +112,7 @@ function OngoingEvents({ filterType, filterAvailable }: OngoingEventsProps) {
   return (
     <div className="events">
       {filteredList.map((event, index) => {
-        const props: EventCardProps = {
+        const props: EventListItemType = {
           ...event,
         };
         return <EventCard key={index} {...props} />;
@@ -137,3 +131,4 @@ function OngoingEvents({ filterType, filterAvailable }: OngoingEventsProps) {
 }
 
 export default OngoingEvents;
+
