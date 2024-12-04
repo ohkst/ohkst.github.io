34f86f1d3eb66688f6116ccea3dbb7ed3d5bf706
112761 <112761@koreainvestment.com>
2024-12-04 12:12:19 +0900

    쿠폰 페이지 랜딩 로직 변경

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -14,22 +14,23 @@ function Banner({
   }: BannerProps) {
     console.log('banner');
 
-    const handleBannerClick = () => {
+    const handleBannerClick = (destination: string) => {
       if (window.Android) {
         window.Android.moveScreen(destination);
-      } else if (window.webkit && window.webkit.messageHandlers) {
-        // iOS 네이티브 함수 호출
-        if (window.webkit.messageHandlers.moveScreen) {
-          window.webkit.messageHandlers.moveScreen.postMessage(destination);
-        }
-      } else {
-        console.warn("Mobile 환경이 아님");
       }
-    };
+      else if (window.webkit && window.webkit.messageHandlers) {
+          // iOS 네이티브 함수 호출
+          if (window.webkit.messageHandlers.moveScreen) {
+            window.webkit.messageHandlers.moveScreen.postMessage(destination);
+          }
+        } else {
+          console.warn(destination);
+        }
+      };
 
     return (
       <label>
-        <img className={bannerName} onClick={() => handleBannerClick} alt=''/>
+        <img className={bannerName} onClick={() => handleBannerClick(destination)} alt=''/>
       </label>
     );
   }
