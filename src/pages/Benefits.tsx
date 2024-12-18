79dd38aa6c4becce62c57979f0f96cd9953765c3
이건주 <112761@koreainvestment.com>
2024-12-18 13:34:34 +0900

    [이건주] 혜택탭 랜딩 개발

--- src/pages/Benefits.tsx
+++ src/pages/Benefits.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/Benefit.css';
 
 function Benefits() {
+
     const handleBannerClick = (destination: string, openData: string) => {
         if (window.Android) {
           window.Android.moveScreen(`${destination}◆${openData}`);
@@ -15,14 +16,16 @@ function Benefits() {
             console.warn("Mobile 환경이 아님");
           }
         };
+
+        //getOverseasStock
     return (
         <div className="benefits">
             <div id = "benefitBg">
                 <div className = "btnGroup">
                     <button className="content-btn btn1" type="button" >대상 여부 조회</button>
                     <button className="content-btn btn2" type="button" onClick={() => handleBannerClick("3911","")}>해외거래서비스 신청하기</button>
-                    <button className="content-btn btn3" type="button" >이벤트 신청하기</button>
-                    <button className="content-btn btn4" type="button" >주식 추첨하기</button>
+                    <button className="content-btn btn3" type="button" onClick={() => handleBannerClick("4707","openEvent:477")}>이벤트 신청하기</button>
+                    <button className="content-btn btn4" type="button" onClick={() => handleBannerClick("4707","openEvent:784")}>주식 추첨하기</button>
                     <button className="content-btn btn5" type="button" >미참가중</button>
                 </div>
             </div>
